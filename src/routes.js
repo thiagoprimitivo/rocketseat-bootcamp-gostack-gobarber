2257bc41122dc04a69a3ed4b7cdcfe43cef2f24d
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-21 22:19:06 -0300

    Criaçao do middleware de autenticaçao

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
+routes.use(authMiddleware);
+
 routes.post('/sessions', SessionController.store);
 
 export default routes;
