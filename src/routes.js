77a8eb43529aead1e8cc0b9ee47935e68aeeb474
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-21 22:55:14 -0300

    update do usuario

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,9 @@ import SessionController from './app/controllers/SessionController';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
-
-routes.post('/sessions', SessionController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
