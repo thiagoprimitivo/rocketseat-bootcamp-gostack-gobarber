e39b37f848a13fa137f75dc40a123561765c59af
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-27 21:58:40 -0300

    Agendamento de serviço

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,11 @@ import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
-import UserController from './app/controllers/UserController';
-import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
