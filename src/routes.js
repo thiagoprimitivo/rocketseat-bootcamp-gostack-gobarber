d08ab9a22fd049338f8f5ccef13f2fc79261f4fb
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-29 12:20:28 -0300

    Listando agendamentos do prestador de serviço

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,11 +19,15 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
