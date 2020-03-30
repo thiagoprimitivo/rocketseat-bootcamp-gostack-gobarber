24462610c6d43100b6bb033867c1bb80e672bc2c
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-30 16:27:59 -0300

    Configurando o Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '42cd460aca1a2f',
+    pass: 'ce8dc1b7682d46',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
