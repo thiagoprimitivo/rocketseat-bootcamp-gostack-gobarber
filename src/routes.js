77584efa0cf3aaba7b2499813822e6b4a4e44d1b
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-03-31 14:46:51 -0300

    Listando horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import ProviderController from './app/controllers/ProviderController';
+import AvailableController from './app/controllers/AvailableController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
@@ -25,6 +26,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
